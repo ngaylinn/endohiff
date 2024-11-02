2df44f2b5dc392bbb623166f12943d6d5c7781de
Nate Gaylinn <nate.gaylinn@gmail.com>
2024-11-02 11:21:00 -0400

    Fix a subtle bug in HIFF evaluation.

--- hiff.py
+++ hiff.py
@@ -12,7 +12,7 @@ def weighted_hiff(bitstr, weights):
     # For all the powers of two up to BITSTR_LEN, look at all the substrings of
     # that size. We start with length 2, since we handled length 1 above.
     substr_len = 2
-    mask = 0b11
+    mask = ti.cast(0b11, BITSTR_DTYPE)
     w = 0
     for p in range(BITSTR_POWER):
         # Use a bitmask as a sliding window, isolating each substring in turn.
@@ -45,7 +45,7 @@ def hiff_demo(bitstr: BITSTR_DTYPE, weights: ti.template()):
 
 
 if __name__ == '__main__':
-    ti.init(ti.cuda)
-    bitstr = 0b11100100000111110000000000000000
+    ti.init(ti.cpu, cpu_max_num_threads=1)
+    bitstr = 0b1111111111111111000000000000000011111111111111110000000000000000
     weights = ti.Vector([1.0] * NUM_WEIGHTS)
     hiff_demo(bitstr, weights)
