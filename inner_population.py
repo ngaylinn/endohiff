4227a750235b8c3e666507b31f58cf1d611d39ff
Nate Gaylinn <nate.gaylinn@gmail.com>
2024-11-02 13:04:00 -0400

    Add migration (just one variant for now).

--- inner_population.py
+++ inner_population.py
@@ -92,25 +92,75 @@ class InnerPopulation:
         self.pop_sum_fitness[g] = fitness_sum  # Assigning to the field
 
     @ti.kernel
-    def propagate(self, environment: ti.template(), g: int):
-        # TODO: For now, this function implements mortality and random
-        # mutation, but nothing more advanced like biasing towards more fit
-        # individuals, crossover, or migration.
+    def migrate(self, g: int):
+        # TODO: Try out different migration strategies!
+        # Ways to do migration:
+        # - When does migration happen?
+        #   - Before / after reproduction?
+        #   - # of migrations per generation?
+        # - How do individuals move?
+        #   - "Acorn drop"
+        #   - "Walk" to adjacent cell
+        #   - Random shuffling
+        #   - Move, or clone?
+        # - How do you resolve competition?
+        #   - Reserve some spots for migrants
+        #   - Fill empty spots first
+        #   - Let migrants overwrite locals
+        #   - Replace local only if fitter
+        # - How selective?
+        #   - Random / more fit individuals migrate
+        #   - Replace random / less fit individuals
         for x, y, i in ti.ndrange(*self.shape):
-            parent = self.pop[g, x, y, i]
+            # This magic number was calculated by brute force such that two
+            # samples from a standard normal distribution will be (0, 0) 90% of
+            # the time (meaning, migration happens 10% of the time).
+            scale_factor = 0.5
 
-            # If this cell was unoccupied in the last generation...
-            if parent.id == DEAD_ID:
+            # "Acorn drop": draw from a standard normal distribution to find a
+            # new home for this individual.
+            dx = ti.round(scale_factor*ti.randn())
+            dy = ti.round(scale_factor*ti.randn())
+
+            # Make sure the new location is in bounds. This might cause
+            # migrants to "pile up" at the edges of the environment.
+            new_x = int(ti.math.clamp(x + dx, 0, ENVIRONMENT_SHAPE[0]))
+            new_y = int(ti.math.clamp(y + dy, 0, ENVIRONMENT_SHAPE[1]))
+            new_i = ti.random(ti.int32) % CARRYING_CAPACITY
+
+            if new_x != x and new_y != y:
+                # Move this individual to a random spot in a nearby location,
+                # possibly overwriting an existing individual there. Then,
+                # leave this spot empty.
+                individual = self.pop[g, x, y, i]
+                self.pop[g, new_x, new_y, new_i] = individual
+                self.pop[g, x, y, i] = DEAD
+
+    @ti.kernel
+    def refill_empty_spaces(self, g: int):
+        for x, y, i in ti.ndrange(*self.shape):
+            individual = self.pop[g, x, y, i]
+
+            # If this spot was unoccupied last generation...
+            if individual.id == DEAD_ID:
                 # Maybe let another individual spawn into this cell.
                 if ti.random() < self.refill_rate:
-                    # Either pick a parent at random from the sub population at
-                    # this location, or do tournament selection to pick a more
-                    # fit parent.
+                    # Either pick a random individual from the sub population
+                    # at this location, or do tournament selection to pick a
+                    # more fit individual.
                     if self.random_refill:
-                        parent_index = ti.random(int) %  CARRYING_CAPACITY
-                        parent = self.pop[g, x, y, parent_index]
+                        new_index = ti.random(int) %  CARRYING_CAPACITY
+                        individual = self.pop[g, x, y, new_index]
                     else:
-                        parent = tournament_selection(self.pop, g, x, y)
+                        individual = tournament_selection(self.pop, g, x, y)
+
+            self.pop[g, x, y, i] = individual
+
+
+    @ti.kernel
+    def populate_children(self, environment: ti.template(), g: int):
+        for x, y, i in ti.ndrange(*self.shape):
+            parent = self.pop[g, x, y, i]
 
             # If this individual isn't fit enough to survive here...
             if parent.fitness < environment.min_fitness[x, y]:
@@ -137,7 +187,18 @@ class InnerPopulation:
                 # Place the child in the next generation
                 self.pop[g + 1, x, y, i] = child
 
+        # Increment the next_id by the maximum number of individuals we might
+        # have populated this generation. There may be unused ids, but that's
+        # fine, we just care that ids are never reused.
         self.next_id[None] += ti.static(MAX_POPULATION_SIZE)
+
+
+    def propagate(self, environment, generation):
+        # TODO: Experiment with different orders...
+        self.migrate(generation)
+        self.refill_empty_spaces(generation)
+        self.populate_children(environment, generation)
+
 
     def to_numpy(self):
         return self.pop.to_numpy()
