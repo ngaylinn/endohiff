fa08a0d6d4bf51d392d2b4511c507fb48e6716d5
Nate Gaylinn <nate.gaylinn@gmail.com>
2024-11-24 09:12:50 -0500

    Generate head-to-head hiff distribution comparisons for supplemental experiments.

--- baym_variants.py
+++ baym_variants.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
+import seaborn as sns
+from chart_across_experiments import compare_hiff_distributions
 
-from constants import ENVIRONMENT_SHAPE, INNER_GENERATIONS, OUTPUT_PATH
+from constants import ENVIRONMENT_SHAPE, INNER_GENERATIONS, OUTPUT_PATH, MAX_HIFF
 from chart import chart_hiff_dist
 from environment import Environment, make_baym
 from evolve import evolve
@@ -45,6 +48,8 @@ def baym_variants():
         'stretched': make_stretched,
         'no_ramp': make_noramp,
     }
+    logs = {}
+
     for name, make_env in variants.items():
         path = OUTPUT_PATH / 'baym_variants' / name
         path.mkdir(exist_ok=True, parents=True)
@@ -55,6 +60,7 @@ def baym_variants():
         last_gen = inner_log.filter(
             pl.col('Generation') == INNER_GENERATIONS - 1
         )
+        logs[name] = inner_log
 
         save_env_map(path, name, env.to_numpy())
         save_hiff_map(path, name, last_gen)
@@ -67,6 +73,30 @@ def baym_variants():
             # If the population dies out immediately, rendering a distribution
             # chart fails, but that's okay. Just skip that visualization.
             pass
+
+    # Compare the hiff distributions for the baym and stretched variants head
+    # to head (we leave out the no_ramp variant, since the population dies out)
+    head_to_head = pl.concat((
+        logs['baym'].with_columns(environment=pl.lit('baym')),
+        logs['stretched'].with_columns(environment=pl.lit('stretched'))
+    )).filter(
+        (pl.col('id') > 0) &
+        (pl.col('Generation') == INNER_GENERATIONS - 1)
+    ).group_by(
+        'environment', 'x', 'y'
+    ).agg(
+        pl.col('hiff').mean().alias('Mean Hiff')
+    )
+
+    fig = sns.displot(
+        head_to_head, x='Mean Hiff', kind='kde', hue='environment', aspect=1.33)
+    plt.xlim(200, MAX_HIFF)
+    sns.move_legend(fig, 'upper right', bbox_to_anchor=(0.60, 0.8))
+    fig.set(title=f'Population HIFF Distribution (baym variants)')
+    fig.savefig(OUTPUT_PATH / 'baym_variants'/ f'hiff_dist.png', dpi=600)
+    plt.close()
+    with open(OUTPUT_PATH / 'baym_variants' / 'mannwitneyu.txt', 'w') as file:
+        compare_hiff_distributions(file, head_to_head, 'environment')
 
 
 if __name__ == '__main__':
--- selection_pressure.py
+++ selection_pressure.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
 import polars as pl
+import seaborn as sns
+from chart_across_experiments import compare_hiff_distributions
 
 import constants
-from constants import OUTPUT_PATH, INNER_GENERATIONS
+from constants import OUTPUT_PATH, INNER_GENERATIONS, MAX_HIFF
 
 constants.TOURNAMENT_SIZE = 10
 
@@ -17,6 +20,7 @@ def selection_pressure():
         'baym': make_baym,
         'flat': make_flat,
     }
+    logs = {}
     for name, make_env in variants.items():
         path = OUTPUT_PATH / 'selection_pressure' / name
         path.mkdir(exist_ok=True, parents=True)
@@ -24,12 +28,35 @@ def selection_pressure():
         env = make_env()
         inner_population = InnerPopulation()
         inner_log, _, _ = evolve(inner_population, env, True, True)
+        logs[name] = inner_log
         last_gen = inner_log.filter(
             pl.col('Generation') == INNER_GENERATIONS - 1
         )
 
         save_hiff_map(path, name, last_gen)
         chart_hiff_dist(path, name, inner_log)
+
+    head_to_head = pl.concat((
+        logs['baym'].with_columns(environment=pl.lit('baym')),
+        logs['flat'].with_columns(environment=pl.lit('flat'))
+    )).filter(
+        (pl.col('id') > 0) &
+        (pl.col('Generation') == INNER_GENERATIONS - 1)
+    ).group_by(
+        'environment', 'x', 'y'
+    ).agg(
+        pl.col('hiff').mean().alias('Mean Hiff')
+    )
+
+    fig = sns.displot(
+        head_to_head, x='Mean Hiff', kind='kde', hue='environment', aspect=1.33)
+    plt.xlim(200, MAX_HIFF)
+    sns.move_legend(fig, 'upper right', bbox_to_anchor=(0.40, 0.8))
+    fig.set(title=f'Population HIFF Distribution (baym variants)')
+    fig.savefig(OUTPUT_PATH / 'selection_pressure'/ f'hiff_dist.png', dpi=600)
+    plt.close()
+    with open(OUTPUT_PATH / 'selection_pressure' / 'mannwitneyu.txt', 'w') as file:
+        compare_hiff_distributions(file, head_to_head, 'environment')
 
 
 if __name__ == '__main__':
